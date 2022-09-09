f4acf46c6fbaf50ecbd08d2b113f1ca179fdebff
serge <sergei.g.d@gmail.ccom>
2022-09-09 18:06:50 +0700

    готово

--- src/main/java/pro/sky/hw2/StringList/StringList.java
+++ src/main/java/pro/sky/hw2/StringList/StringList.java
@@ -1,4 +1,27 @@
 package pro.sky.hw2.StringList;
 
-public class StringList {
+public interface StringList {
+    String add(String item);
+    String add(int index, String item);
+    String set(int index, String item);
+
+    String remove(String item);
+    String remove(int index);
+    boolean contains(String item);
+
+    int indexOf(String item);
+
+    int lastIndexOf(String item);
+
+    String get(int index);
+
+    boolean equals(StringList otherList);
+
+    int size();
+
+    boolean isEmpty();
+
+    void clear();
+
+    String[] toArray();
 }
---
+++ src/main/java/pro/sky/hw2/StringList/exception/ItemNullException.java
@@ -0,0 +1,4 @@
+package pro.sky.hw2.StringList.exception;
+
+public class ItemNullException extends RuntimeException{
+}
